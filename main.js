ee95bd6cb94d38bda4b371d0e8834b1f77121a47
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-12-31 15:29:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
 song=loadSound("music.mp3");
 }
@@ -8,14 +12,33 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',getPoses);
 }
+function modelloaded(){
+    console.log("PoseNet Initialized");
+}
+function getPoses(results){
+ if(results.length > 0){
+    console.log(results);
+    
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("Left Wrist X = "+leftWristX+" , "+"Left Wrist Y = "+leftWristY);
+
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("Right Wrist X = "+rightWristX+" , "+"Right Wrist Y = "+rightWristY);
+ }
+}
+
 function draw(){
     image(video,0,0,600,600)
 }
 function play(){
     song.play();
-    song.volume(0.5);
-    song.rate(0.4)
+    song.setVolume(0.5);
+    song.rate(1);
 }
 function stop(){
     song.stop();
